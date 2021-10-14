e2b6b12f87096b18ad3de305466927df4b40cd82
Samuelcodingg <samuelroman39@gmail.com>
2021-10-14 11:08:06 -0500

    maquetado de footer iniciado

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="bg-ieee py-4">
+            <ul className="w-25 mx-auto text-white d-flex align-items-center justify-content-evenly">
+                <li><i class="fab fa-facebook fs-1"></i></li>
+                <li><i class="far fa-envelope fs-1"></i></li>
+                <li><i class="fab fa-linkedin-in fs-1"></i></li>
+                <li><i class="fab fa-instagram fs-1"></i></li>
+            </ul>
+        </footer>
     )
 }
