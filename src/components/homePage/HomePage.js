de8da83c128ba1ddece1953e8410060f82fb50e1
Manuel Vallejos <manuel.vallejos@unmsm.edu.pe>
2021-10-19 19:13:51 -0500

    Slider de las novedades en Home

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import { Carousel } from './Carousel'
 import { EventCard } from './EventCard'
+import { CarouselNov } from './CarouselNov'
 
 export const HomePage = () => {
     return (
         <div>
 
             <Carousel />
+            
 
+            
             <div className="py-5 bg-ieee">
-                <h3 className="text-center mb-4 text-white" style={{paddingBottom:"1rem"}}>Novedades</h3>
-                <div className="container">
-                    <div className="row">
-                        <EventCard />
-                        <EventCard />
-                        <EventCard />
-                    </div>
-                </div>
+                <h3 className="text-center mb-4 text-white">Novedades</h3>
+                <CarouselNov/>
             </div>
-
 
             <div className="py-5">
                 <h3 className="text-center mb-4" style={{paddingBottom:"1rem"}}>Nuestros Proyectos</h3>
