9d087c0e083573a5916feff21e894dd49b470979
Manuel Vallejos <manuel.vallejos@unmsm.edu.pe>
2021-10-24 06:07:11 -0500

    FAQS casi listo

--- src/components/faqsPage/FaqsPage.js
+++ src/components/faqsPage/FaqsPage.js
@@ -3,7 +3,7 @@ import { ContentModelBanner } from '../ui/ContentModelBanner';
 import img from '../contactPage/programming-banner.jpg';
 import { QuestionAccordion } from './QuestionAccordion';
 
-/*Aún falta agregarle funcionalidades con js y modificar algo de estilos*/ 
+/*Aún falta agregarle algo de estilos, quizá una sola funcionalidad: Que siempre haya un set de preguntas abierto*/ 
 export const FaqsPage = () => {
 
     const modifyColor = () => {
@@ -14,10 +14,8 @@ export const FaqsPage = () => {
                 $btnsSelect[i].classList.add("btn-select-color");
               
             }
-
             if ($btnsSelect[i].getAttribute("aria-expanded") === "false") {
                 $btnsSelect[i].classList.remove("btn-select-color");
-
                 
             }
         }
@@ -35,12 +33,6 @@ export const FaqsPage = () => {
     return (
         <div>
             <ContentModelBanner title="Preguntas frecuentes" subtitle="Te ayudamos a resolver tus dudas" p="En caso no encuentre su pregunta puede contactase con nosotros mediante nuestras redes sociales" img={img} />
-
-            <h3>Accordion expandido solucionado</h3>
-            <h3>Falta solucionar: Cuando una de las secciones se expande, otro se debe acortar
-                La posible solución es el .show raaa.
-                Probablemente la solución sea meterle accordion
-            </h3>
 
             <div className="w-100" style={{minHeight:"85vh"}}>
                 <div className="container" style={{marginTop:"3rem", paddingBottom:"0.3rem"}}>
@@ -61,112 +53,112 @@ export const FaqsPage = () => {
                                 </li>
                             </ul>
                         </div>
-                        <div className="col-sm-8">
-                            <div className="collapse show collapse-questions-item" id="collapseExample1">
+                        <div className="col-sm-8" id="raaa">
+                            <div className="collapse show collapse-questions-item" id="collapseExample1" data-bs-parent="#raaa">
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T1"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question1"
                                         idAnswer="answer1"
                                         idParent="collapseExample1"
                                         />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T1"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question2"
                                         idAnswer="answer2"
                                         idParent="collapseExample1"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T1"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question3"
                                         idAnswer="answer3"
                                         idParent="collapseExample1"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T1"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question4"
                                         idAnswer="answer4"
                                         idParent="collapseExample1"
                                     />
                             
                             </div>
-                            <div className="collapse collapse-questions-item" id="collapseExample2">
+                            <div className="collapse collapse-questions-item" id="collapseExample2" data-bs-parent="#raaa">
                             
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T2"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question5"
                                         idAnswer="answer5"
                                         idParent="collapseExample2"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T2"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question6"
                                         idAnswer="answer6"
                                         idParent="collapseExample2"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T2"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question7"
                                         idAnswer="answer7"
                                         idParent="collapseExample2"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T2"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question8"
                                         idAnswer="answer8"
                                         idParent="collapseExample2"
                                     />
                             
                             </div>
-                            <div className="collapse collapse-questions-item" id="collapseExample3">
+                            <div className="collapse collapse-questions-item" id="collapseExample3" data-bs-parent="#raaa">
                             
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T3"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question9"
                                         idAnswer="answer9"
                                         idParent="collapseExample3"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T3"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question10"
                                         idAnswer="answer10"
                                         idParent="collapseExample3"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T3"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question11"
                                         idAnswer="answer11"
                                         idParent="collapseExample3"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T3"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question12"
                                         idAnswer="answer12"
                                         idParent="collapseExample3"
                                     />
                                 
                             </div>
-                            <div className="collapse collapse-questions-item" id="collapseExample4">
+                            <div className="collapse collapse-questions-item" id="collapseExample4" data-bs-parent="#raaa">
                                 
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T4"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question1"
                                         idAnswer="answer1"
                                         idParent="collapseExample4"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T4"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question2"
                                         idAnswer="answer2"
                                         idParent="collapseExample4"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T4"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question3"
                                         idAnswer="answer3"
                                         idParent="collapseExample4"
                                     />
                                     <QuestionAccordion
-                                        question="¿Hola Bola?"
+                                        question="¿Hola Bola? T4"
                                         answer="lorem lorem lorem gaa el que no salta se kcha al kbro" idQuestion="question4"
                                         idAnswer="answer4"
                                         idParent="collapseExample4"
@@ -184,24 +176,4 @@ export const FaqsPage = () => {
         
     )
 }
-
-/*setInterval(() => {
-    console.log(document.querySelector(".btn-select").getAttribute("aria-expanded"));
-},1000)*/
-
-//console.log($btnsSelect.getAtributtes());
-
-
-
-
-
-
-
-/*
-setInterval(() => {
-    modifyColor();
-    console.log("Hola");
-}, 1000);
-
-*/
 
